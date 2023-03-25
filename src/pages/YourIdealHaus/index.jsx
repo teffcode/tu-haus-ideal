218da5abf3e963064fed803d06e0ff65c8980aeb
teffcode <estefanyaguilar@lahaus.com>
2023-03-24 20:45:06 -0500

    add: greeting animation

--- src/pages/YourIdealHaus/index.jsx
+++ src/pages/YourIdealHaus/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import PrimaryButton from '../../components/primaryButton'
 import Logo from '../../components/Logo'
 import { HausContext } from '../App'
@@ -7,11 +7,30 @@ import './styles.css'
 const YourIdealHaus = () => {
   const context = useContext(HausContext)
 
+  useEffect(() => {
+    let i = 0
+    let tag = document.getElementById('text')
+    let html = document.getElementById('text').innerHTML
+    let attr = tag.setAttribute('data', html)
+    let txt = tag.getAttribute('data')
+    let speed = 80
+
+    const typeWriter = () => {
+      if (i <= txt.length) {
+        tag.innerHTML = txt.slice(0 , i + 1)
+        i++
+        setTimeout(typeWriter, speed)
+      }
+    }
+
+    typeWriter()
+  })
+
   const renderWelcome = () => {
     return (
       <div className='welcome inline-flex flex-col items-center w-full h-full p-6'>
-        <div className='flex flex-1'>
-          <p className='self-end text-center text-white mb-4'>
+        <div className='welcome__text flex flex-1 mb-4'>
+          <p id="text" className='self-end text-white text-center'>
             ¡Bienvenido! Aquí te ayudaremos a encontrar tu propiedad ideal. Responde algunas preguntas y te guiaremos. ¡Eres un inversionista, perfecto!
           </p>
         </div>
