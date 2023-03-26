b61528cbffb98b23ea7a3f81f8435a605edbaf95
teffcode <estefanyaguilar@lahaus.com>
2023-03-25 21:24:48 -0500

    add: request options

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,1 +1,13 @@
 export const QUESTIONS_API = 'https://ms-lh-hackathon-2023.onrender.com/api/questions'
+
+export const REQUEST_OPTIONS = (body) => {
+  return ({
+    method: 'POST',
+    mode: 'no-cors',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+}
